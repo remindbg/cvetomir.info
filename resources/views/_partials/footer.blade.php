428d2322f0ba5ccea591304a426f08da614172ce
remindbg <alanforts@gmail.com>
2018-11-02 20:06:16 +0200

    fixed dropdown

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,4 +1,4 @@
-<script src="{{URL::Asset('vendor/jquery/jquery.min.js')}}" async="async"></script>
+<script src="{{URL::Asset('vendor/jquery/jquery.min.js')}}"></script>
 <script src="{{URL::Asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}" defer async="async"></script>
 <script src="{{URL::Asset('vendor/tinymce/plugins/codesample/prism.min.js')}}" defer async="async"></script>
 @yield('scripts')
