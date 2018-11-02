58b0f13bb54256caa3e342df08c44a867d79c02c
remindbg <alanforts@gmail.com>
2018-11-02 08:48:55 +0200

    fix category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,9 +8,10 @@ class CategoryController extends Controller
 {
     public function single($id) {
         $category = Category::findOrFail($id)->with('articles')->get()->first();
-        $articles = $category->articles;
-        return view('articles.category',compact('category','articles'));
+       // $articles = $category->articles;
+        $articles = Category::find($id)->articles;
 
+        return view('articles.category',compact('category','articles'));
 
     }
 }
--- resources/views/articles/category.blade.php
+++ resources/views/articles/category.blade.php
@@ -17,8 +17,17 @@
                 <a href="/articles/{{$article->id}}/{{$article->slug}}"><h4 class="">{{$article->title}}</h4></a>
                 <p class="small text-muted"> Прегледа {{$article->views}} |  Коментари  | Категория: <a href="/category/{{$article->category->id}}/{{$article->category->slug}}">{{$article->category->title}}</a></p>
                 <div class="row">
+                    <div class="col-lg-3">
+                        @if($article->image)
+                            <div class="row justify-content-center">
+
+                                <img class="img-thumbnail" src="{{url('/images/articles/'.$article->image)}}" style="width: 180px; height: 100px;">
+
+                            </div>
+                        @endif
+                    </div>
                     <div class="col-lg-9">
-                        <p class="card-text">{!! strip_tags($article->body) !!}</p>
+                        <p class="card-text">{!! str_limit(strip_tags($article->body),250) !!}</p>
                     </div>
                 </div>
             </div>
